568f00ecacdc9547865fd82ad87f196c5984eaf5
Acepmon <dtsogtbayar123@gmail.com>
2024-11-13 16:28:35 +0800

    ssr set to true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,33 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
+
+  ssr: true,
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@mockline/utils',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxthq/studio'
+    "@nuxt/content",
+    "@mockline/utils",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxthq/studio",
   ],
 
   routeRules: {
-    '/': { isr: true, prerender: true },
+    "/": { isr: true, prerender: true },
   },
 
   colorMode: {
-    preference: 'light',
-    fallback: 'light',
-    storageKey: 'cv-color-mode',
+    preference: "light",
+    fallback: "light",
+    storageKey: "cv-color-mode",
   },
 
   studio: {
-    enabled: true
-  }
-})
+    enabled: true,
+  },
+});
